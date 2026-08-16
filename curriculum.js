b430bb76167c0ba4eb2bf3dc5cf63518d205139f
kvijaya18041977-oss <kvijaya18041977@gmail.com>
2026-08-16 16:46:20 +0530

    Update curriculum.js

--- curriculum.js
+++ curriculum.js
@@ -1,1 +1,101 @@
+const curriculum = {
 
+    1: {
+        "தமிழ்": [],
+        "English": [],
+        "Mathematics": [],
+        "Science": []
+    },
+
+    2: {
+        "தமிழ்": [],
+        "English": [],
+        "Mathematics": [],
+        "Science": []
+    },
+
+    3: {
+        "தமிழ்": [],
+        "English": [],
+        "Mathematics": [],
+        "Science": [],
+        "Social Science": []
+    },
+
+    4: {
+        "தமிழ்": [],
+        "English": [],
+        "Mathematics": [],
+        "Science": [],
+        "Social Science": []
+    },
+
+    5: {
+        "தமிழ்": [],
+        "English": [],
+        "Mathematics": [],
+        "Science": [],
+        "Social Science": []
+    },
+
+    6: {
+        "தமிழ்": [],
+        "English": [],
+        "Mathematics": [],
+        "Science": [],
+        "Social Science": []
+    },
+
+    7: {
+        "தமிழ்": [],
+        "English": [],
+        "Mathematics": [],
+        "Science": [],
+        "Social Science": []
+    },
+
+    8: {
+        "தமிழ்": [],
+        "English": [],
+        "Mathematics": [],
+        "Science": [],
+        "Social Science": []
+    },
+
+    9: {
+        "தமிழ்": [],
+        "English": [],
+        "Mathematics": [],
+        "Science": [],
+        "Social Science": []
+    },
+
+    10: {
+        "தமிழ்": [],
+        "English": [],
+        "Mathematics": [],
+        "Science": [],
+        "Social Science": []
+    },
+
+    11: {
+        "தமிழ்": [],
+        "English": [],
+        "Mathematics": [],
+        "Physics": [],
+        "Chemistry": [],
+        "Biology": [],
+        "Computer Science": []
+    },
+
+    12: {
+        "தமிழ்": [],
+        "English": [],
+        "Mathematics": [],
+        "Physics": [],
+        "Chemistry": [],
+        "Biology": [],
+        "Computer Science": []
+    }
+
+};
