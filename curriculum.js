d5e56d36db996b7d8ac1ca5e035f9f0db3872a14
kvijaya18041977-oss <kvijaya18041977@gmail.com>
2026-08-16 19:24:11 +0530

    Update curriculum.js

--- curriculum.js
+++ curriculum.js
@@ -1,101 +1,348 @@
 const curriculum = {
 
     1: {
-        "தமிழ்": [],
-        "English": [],
-        "Mathematics": [],
-        "Science": []
+        "தமிழ்": {
+            book: "Class 1 தமிழ்",
+            chapters: []
+        },
+
+        "English": {
+            book: "Class 1 English",
+            chapters: []
+        },
+
+        "Mathematics": {
+            book: "Class 1 Mathematics",
+            chapters: []
+        },
+
+        "Science": {
+            book: "Class 1 Science",
+            chapters: []
+        }
     },
+
 
     2: {
-        "தமிழ்": [],
-        "English": [],
-        "Mathematics": [],
-        "Science": []
+        "தமிழ்": {
+            book: "Class 2 தமிழ்",
+            chapters: []
+        },
+
+        "English": {
+            book: "Class 2 English",
+            chapters: []
+        },
+
+        "Mathematics": {
+            book: "Class 2 Mathematics",
+            chapters: []
+        },
+
+        "Science": {
+            book: "Class 2 Science",
+            chapters: []
+        }
     },
+
 
     3: {
-        "தமிழ்": [],
-        "English": [],
-        "Mathematics": [],
-        "Science": [],
-        "Social Science": []
+        "தமிழ்": {
+            book: "Class 3 தமிழ்",
+            chapters: []
+        },
+
+        "English": {
+            book: "Class 3 English",
+            chapters: []
+        },
+
+        "Mathematics": {
+            book: "Class 3 Mathematics",
+            chapters: []
+        },
+
+        "Science": {
+            book: "Class 3 Science",
+            chapters: []
+        },
+
+        "Social Science": {
+            book: "Class 3 Social Science",
+            chapters: []
+        }
     },
+
 
     4: {
-        "தமிழ்": [],
-        "English": [],
-        "Mathematics": [],
-        "Science": [],
-        "Social Science": []
+        "தமிழ்": {
+            book: "Class 4 தமிழ்",
+            chapters: []
+        },
+
+        "English": {
+            book: "Class 4 English",
+            chapters: []
+        },
+
+        "Mathematics": {
+            book: "Class 4 Mathematics",
+            chapters: []
+        },
+
+        "Science": {
+            book: "Class 4 Science",
+            chapters: []
+        },
+
+        "Social Science": {
+            book: "Class 4 Social Science",
+            chapters: []
+        }
     },
+
 
     5: {
-        "தமிழ்": [],
-        "English": [],
-        "Mathematics": [],
-        "Science": [],
-        "Social Science": []
+        "தமிழ்": {
+            book: "Class 5 தமிழ்",
+            chapters: []
+        },
+
+        "English": {
+            book: "Class 5 English",
+            chapters: []
+        },
+
+        "Mathematics": {
+            book: "Class 5 Mathematics",
+            chapters: []
+        },
+
+        "Science": {
+            book: "Class 5 Science",
+            chapters: []
+        },
+
+        "Social Science": {
+            book: "Class 5 Social Science",
+            chapters: []
+        }
     },
+
 
     6: {
-        "தமிழ்": [],
-        "English": [],
-        "Mathematics": [],
-        "Science": [],
-        "Social Science": []
+        "தமிழ்": {
+            book: "Class 6 தமிழ்",
+            chapters: []
+        },
+
+        "English": {
+            book: "Class 6 English",
+            chapters: []
+        },
+
+        "Mathematics": {
+            book: "Class 6 Mathematics",
+            chapters: []
+        },
+
+        "Science": {
+            book: "Class 6 Science",
+            chapters: []
+        },
+
+        "Social Science": {
+            book: "Class 6 Social Science",
+            chapters: []
+        }
     },
+
 
     7: {
-        "தமிழ்": [],
-        "English": [],
-        "Mathematics": [],
-        "Science": [],
-        "Social Science": []
+        "தமிழ்": {
+            book: "Class 7 தமிழ்",
+            chapters: []
+        },
+
+        "English": {
+            book: "Class 7 English",
+            chapters: []
+        },
+
+        "Mathematics": {
+            book: "Class 7 Mathematics",
+            chapters: []
+        },
+
+        "Science": {
+            book: "Class 7 Science",
+            chapters: []
+        },
+
+        "Social Science": {
+            book: "Class 7 Social Science",
+            chapters: []
+        }
     },
+
 
     8: {
-        "தமிழ்": [],
-        "English": [],
-        "Mathematics": [],
-        "Science": [],
-        "Social Science": []
+        "தமிழ்": {
+            book: "Class 8 தமிழ்",
+            chapters: []
+        },
+
+        "English": {
+            book: "Class 8 English",
+            chapters: []
+        },
+
+        "Mathematics": {
+            book: "Class 8 Mathematics",
+            chapters: []
+        },
+
+        "Science": {
+            book: "Class 8 Science",
+            chapters: []
+        },
+
+        "Social Science": {
+            book: "Class 8 Social Science",
+            chapters: []
+        }
     },
+
 
     9: {
-        "தமிழ்": [],
-        "English": [],
-        "Mathematics": [],
-        "Science": [],
-        "Social Science": []
+        "தமிழ்": {
+            book: "Class 9 தமிழ்",
+            chapters: []
+        },
+
+        "English": {
+            book: "Class 9 English",
+            chapters: []
+        },
+
+        "Mathematics": {
+            book: "Class 9 Mathematics",
+            chapters: []
+        },
+
+        "Science": {
+            book: "Class 9 Science",
+            chapters: []
+        },
+
+        "Social Science": {
+            book: "Class 9 Social Science",
+            chapters: []
+        }
     },
+
 
     10: {
-        "தமிழ்": [],
-        "English": [],
-        "Mathematics": [],
-        "Science": [],
-        "Social Science": []
+        "தமிழ்": {
+            book: "Class 10 தமிழ்",
+            chapters: []
+        },
+
+        "English": {
+            book: "Class 10 English",
+            chapters: []
+        },
+
+        "Mathematics": {
+            book: "Class 10 Mathematics",
+            chapters: []
+        },
+
+        "Science": {
+            book: "Class 10 Science",
+            chapters: []
+        },
+
+        "Social Science": {
+            book: "Class 10 Social Science",
+            chapters: []
+        }
     },
+
 
     11: {
-        "தமிழ்": [],
-        "English": [],
-        "Mathematics": [],
-        "Physics": [],
-        "Chemistry": [],
-        "Biology": [],
-        "Computer Science": []
+        "தமிழ்": {
+            book: "Class 11 தமிழ்",
+            chapters: []
+        },
+
+        "English": {
+            book: "Class 11 English",
+            chapters: []
+        },
+
+        "Mathematics": {
+            book: "Class 11 Mathematics",
+            chapters: []
+        },
+
+        "Physics": {
+            book: "Class 11 Physics",
+            chapters: []
+        },
+
+        "Chemistry": {
+            book: "Class 11 Chemistry",
+            chapters: []
+        },
+
+        "Biology": {
+            book: "Class 11 Biology",
+            chapters: []
+        },
+
+        "Computer Science": {
+            book: "Class 11 Computer Science",
+            chapters: []
+        }
     },
 
+
     12: {
-        "தமிழ்": [],
-        "English": [],
-        "Mathematics": [],
-        "Physics": [],
-        "Chemistry": [],
-        "Biology": [],
-        "Computer Science": []
+        "தமிழ்": {
+            book: "Class 12 தமிழ்",
+            chapters: []
+        },
+
+        "English": {
+            book: "Class 12 English",
+            chapters: []
+        },
+
+        "Mathematics": {
+            book: "Class 12 Mathematics",
+            chapters: []
+        },
+
+        "Physics": {
+            book: "Class 12 Physics",
+            chapters: []
+        },
+
+        "Chemistry": {
+            book: "Class 12 Chemistry",
+            chapters: []
+        },
+
+        "Biology": {
+            book: "Class 12 Biology",
+            chapters: []
+        },
+
+        "Computer Science": {
+            book: "Class 12 Computer Science",
+            chapters: []
+        }
     }
 
 };
