249fdcd08cfd725e11f32c57eac0e7517b7abdf9
kvijaya18041977-oss <kvijaya18041977@gmail.com>
2026-08-16 14:20:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+alert("Welcome to ACHIEVERS HUB! 🎓");
+
+const buttons = document.querySelectorAll("button");
+
+buttons[0].onclick = function () {
+    alert("Classes 1–12 section coming soon! 📚");
+};
+
+buttons[1].onclick = function () {
+    alert("Quiz section coming soon! 📝");
+};
+
+buttons[2].onclick = function () {
+    alert("Study Materials coming soon! 📖");
+};
